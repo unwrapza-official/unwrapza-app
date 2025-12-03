d50ff12f91659c96d9383f1854022ac7b3b5815d
Bora <bora.e@icloud.com>
2025-12-03 23:31:44 +0100

    TEMP rename to fix casing conflict

--- src/pages/account/AccountLayout.jsx
+++ src/pages/account/AccountLayout.jsx
@@ -1,4 +1,4 @@
-import AccountSidebar from "../../components/account/accountSidebar"
+import AccountSidebar from "../../components/account/AccountSidebar"
 import { Outlet } from "react-router-dom"
 
 const AccountLayout = () => {
